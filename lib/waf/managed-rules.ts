23564f38e5d91f2a14dc922f45ff7791f30535d5
Guerdon Mukama <gmukama@gmail.com>
2026-04-09 17:18:30 +1000

    fix: update managed rule

--- lib/waf/managed-rules.ts
+++ lib/waf/managed-rules.ts
@@ -1,27 +1,38 @@
 import * as wafv2 from "aws-cdk-lib/aws-wafv2";
 import { ManagedRuleSetConfig } from "../types";
 
-export function buildManagedRule(config: ManagedRuleSetConfig): wafv2.CfnWebACL.RuleProperty {
-  const statement: wafv2.CfnWebACL.ManagedRuleGroupStatementProperty = {
+export function buildManagedRule(
+  config: ManagedRuleSetConfig
+): wafv2.CfnWebACL.RuleProperty {
+  const managedRuleGroupStatement: wafv2.CfnWebACL.ManagedRuleGroupStatementProperty = {
     vendorName: config.vendorName,
     name: config.name,
+    ...(config.version ? { version: config.version } : {}),
+    ...(config.excludedRules && config.excludedRules.length > 0
+      ? {
+        excludedRules: config.excludedRules.map((name) => ({ name })),
+      }
+      : {}),
+    ...(config.name === "AWSManagedRulesBotControlRuleSet" && config.inspectionLevel
+      ? {
+        managedRuleGroupConfigs: [
+          {
+            awsManagedRulesBotControlRuleSet: {
+              inspectionLevel: config.inspectionLevel,
+            },
+          },
+        ],
+      }
+      : {}),
   };
-
-  if (config.version) statement.version = config.version;
-  if (config.excludedRules?.length) {
-    statement.excludedRules = config.excludedRules.map((name) => ({ name }));
-  }
-  if (config.name === "AWSManagedRulesBotControlRuleSet" && config.inspectionLevel) {
-    statement.managedRuleGroupConfigs = [{
-      awsManagedRulesBotControlRuleSet: { inspectionLevel: config.inspectionLevel },
-    }];
-  }
 
   return {
     name: config.name,
     priority: config.priority,
     overrideAction: { none: {} },
-    statement: { managedRuleGroupStatement: statement },
+    statement: {
+      managedRuleGroupStatement,
+    },
     visibilityConfig: {
       cloudWatchMetricsEnabled: true,
       sampledRequestsEnabled: true,
